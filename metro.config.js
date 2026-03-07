4aa22a893b44ad81243aaf6e30c4100631e37b65
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2026-03-07 14:55:03 +0600

    fix: Disable React Compiler, add Reanimated Babel plugin, and refine Expo and Metro configurations.

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,4 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-// Fix: "watcher.unstable_workerThreads" is an unknown option in newer Metro versions
-if (config.watcher) {
-  const { unstable_workerThreads, ...watcherRest } = config.watcher;
-  config.watcher = watcherRest;
-}
-
 module.exports = config;
